b6017c3d71da4b4bbe72a4cfcfce843e7821a86f
Semyon Surkov <semyonsurkov@outlook.com>
2023-04-27 14:52:34 +0300

    Add functions for tabs

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -51,7 +51,7 @@ const modals = () => {
     '.popup_engineer .popup_close'
   );
   bindModal('.phone_link', '.popup', '.popup .popup_close');
-  showModalByTime('.popup', 3000);
+  // showModalByTime('.popup', 60000);
 };
 
 export default modals;
