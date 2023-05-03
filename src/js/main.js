8bff859b006186d20df158426f1701e9a7b89cc0
Semyon Surkov <semyonsurkov@outlook.com>
2023-05-03 10:03:50 +0300

    Add forms.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,20 @@
 import './slider';
-import { modals, tabs } from './modules';
+import { modals, tabs, forms } from './modules';
 
 window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
   modals();
   tabs({
     headerSelector: '.glazing_slider',
     tabSelector: '.glazing_block',
     contentSelector: '.glazing_content',
-    activeClass: 'active'
+    activeClass: 'active',
   });
   tabs({
     headerSelector: '.decoration_slider',
     tabSelector: '.no_click',
     contentSelector: '.decoration_content > div > div',
-    activeClass: 'after_click'
+    activeClass: 'after_click',
   });
+  forms();
 });
---
+++ src/js/modules/forms.js
@@ -0,0 +1,58 @@
+export const forms = () => {
+  const forms = document.querySelectorAll('form');
+  const inputs = document.querySelectorAll('input');
+  const phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+
+  phoneInputs.forEach((input) => {
+    input.addEventListener('input', () => {
+      input.value = input.value.replace(/\D/, '');
+    });
+  });
+
+  const message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы с вами свяжемся',
+    failure: 'Данные не отправлены',
+  };
+
+  const postData = async (url, data) => {
+    document.querySelector('.status').textContent = message.loading;
+    let result = await fetch(url, {
+      method: 'POST',
+      body: data,
+    });
+
+    return await result.text();
+  };
+
+  const clearInputs = () => {
+    inputs.forEach((input) => {
+      input.value = '';
+    });
+  };
+
+  forms.forEach((form) => {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      let statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      form.appendChild(statusMessage);
+
+      const formData = new FormData(form);
+
+      postData('assets/server.php', formData)
+        .then((result) => {
+          console.log(result);
+          statusMessage.textContent = message.success;
+        })
+        .catch(() => (statusMessage.textContent = message.failure))
+        .finally(() => {
+          clearInputs();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 5000);
+        });
+    });
+  });
+};
